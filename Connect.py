5532519b28304eae08db833d5d12c6c5593d2fd2
jinyu jiang <sc19jj@leeds.ac.uk>
2020-07-23 14:18:51 +0100

    add csv

--- Connect.py
+++ Connect.py
@@ -3,21 +3,30 @@
 
 import pymysql
 
+DBHOST = '127.0.0.1'
+DBUSER = 'max'
+DBPASS = 'Jjy662500'
+DBNAME = 'carcdb'
+
 try:
-    db = pymysql.connect('127.0.0.1', 'max', 'Jjy662500', 'carcdb')
-    print("连接成功")
+    db = pymysql.connect( DBHOST, DBUSER, DBPASS, DBNAME)
+    print("Connection succeeded")
     cur = db.cursor()
-    sql = "SELECT * FROM site"
+    sql = "SELECT * FROM doseresp"
     cur.execute(sql)
     results = cur.fetchall()
     for row in results:
-        SiteID = row[0]
-        SiteName = row[1]
-        print ('SiteID:%s, SiteName:%s'%(SiteID, SiteName))
+        DoseRespID = row[0]
+        ObsID = row[1]
+        Dose = row[2]
+        DoseUnit = row[3]
+        TotalAnimals = row[4]
+        WithTumours = row[5]
+
+        print ('DoseRespID:%s, ObsID:%s, Dose:%s, DoseUnit:%s, TotalAnimals:%s,WithTumours:%s'%(DoseRespID, ObsID, Dose, DoseUnit, TotalAnimals,WithTumours))
 
 except pymysql.Error as e:
-    print ("失败" +str(e))
+    print ("Connection failed" +str(e))
     db.rollback()
-
 
 db.close()
